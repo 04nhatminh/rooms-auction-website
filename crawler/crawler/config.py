891c090b53d5e762ab192f553b9dbd67b713ccdd
Tran Khanh Nhu <141507912+Xaoimiimii@users.noreply.github.com>
2025-08-02 10:01:46 +0700

    Update config.py

--- crawler/crawler/config.py
+++ crawler/crawler/config.py
@@ -1,3 +1,3 @@
 USER_AGENT = "Mozilla/5.0"
 API_DOMAIN = "https://www.airbnb.com.vn"
-API_KEY = "d306zoyjsyarp7ifhu67rjxn52tv0t20" # Lấy X-airbnb-api-key từ request header trên airbnb
+API_KEY = "" # Lấy X-airbnb-api-key từ request header trên airbnb
