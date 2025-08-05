9dfbea9396d5155574f231fed9389acf08ead27a
Nhu <gaococonut1199@gmail.com>
2025-08-06 03:16:01 +0700

    Fix house rules and footer - room detail

--- frontend/src/components/Location.js
+++ frontend/src/components/Location.js
@@ -5,8 +5,8 @@ const Location = () => {
   return (
     <div className="location-section">
       <h3>Nơi bạn sẽ đến</h3>
+      <span>Đà Lạt, Lâm Đồng, Việt Nam</span>
       <div className="location-map"></div>
-      <h4>Đà Lạt, Lâm Đồng, Việt Nam</h4>
     </div>
   );
 };
